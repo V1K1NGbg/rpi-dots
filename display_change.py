a0c89bfb402ae69c5461b831ebbe66e64839ace8
V1K1NGbg <victor@ilchev.com>
2024-11-09 20:32:04 +0100

    bug

--- display_change.py
+++ display_change.py
@@ -25,7 +25,7 @@ try:
 
     print(ip)
 
-    draw.text((0, 0),  , font = font24, fill = 0)
+    draw.text((0, 0), ip , font = font24, fill = 0)
     draw.text((10, 20), 'hello world', font = font18, fill = 0)
 
     epd.display(epd.getbuffer(Himage))
