87ce29434a9923609c76ee09ad79106bf72b4d24
V1K1NGbg <victor@ilchev.com>
2024-11-15 23:18:31 +0100

    bug3

--- display_change.py
+++ display_change.py
@@ -43,11 +43,12 @@ try:
     epd.display(epd.getbuffer(Himage))
 
     pressed = False
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup("17", GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.add_event_detect("17", GPIO.FALLING, callback=lambda pin: self.button_pressed(1, button_handler), bouncetime=200)
 
     while pressed == False:
-        if GPIO.input(5) == False:
+        if GPIO.input(17) == False:
             pressed = True
             print("Button Pressed")
             break
