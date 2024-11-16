c7682ff8b6db041e0fd67a0e9a70f039f8f1ae26
V1K1NGbg <victor@ilchev.com>
2024-11-16 19:34:17 +0100

    bug2

--- display_change.py
+++ display_change.py
@@ -82,7 +82,7 @@ try:
     draw(4, ['Hello000', '', 'World',  '...'], font10, lambda draw: (
         # draw.text((53, 8), 'Main box', font=font24, fill=0)
         draw.line((53, 8, 255, 167), fill = 0)
-        draw.line((255, 8, 53, 167), fill = 0)
+        # draw.line((255, 8, 53, 167), fill = 0)
     ))
 
     # Example
