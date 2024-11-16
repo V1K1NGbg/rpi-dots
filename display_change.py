b6294e91e2a69a889f00a02f4e5dfa4bfbd1d077
V1K1NGbg <victor@ilchev.com>
2024-11-16 19:31:35 +0100

    test for main box

--- display_change.py
+++ display_change.py
@@ -78,9 +78,11 @@ try:
     #         print("Button Pressed")
     #         break
 
-    # if n == 0, loc = (8, 8) else loc = (53, 8)
+    # if n == 0, loc = (8, 8) else loc = (53, 8) ; (255, 167)
     draw(4, ['Hello000', '', 'World',  '...'], font10, lambda draw: (
-        draw.text((53, 8), 'Main box', font=font24, fill=0)
+        # draw.text((53, 8), 'Main box', font=font24, fill=0)
+        draw.line((53, 8, 255, 167), fill=0)
+        draw.line((255, 8, 53, 167), fill=0)
     ))
 
     # Example
